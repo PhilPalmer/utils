aff92e050630537334d628f4ed9d28d658b58a11
PhilPalmer <phil.palmer.email@gmail.com>
2019-11-27 14:39:20 +0000

    Added cmd to run bcftools docker container

--- split_cohort_vcf.sh
+++ split_cohort_vcf.sh
@@ -1,4 +1,5 @@
 #!/usr/bin/env bash
+# docker run -it -v $PWD:$PWD -w $PWD halllab/bcftools:v1.9 bash
 for sample in `bcftools query -l sampleA.vcf.gz`
 do
   bcftools view -s $sample -Oz -o ${sample}.vcf.gz sampleA.vcf.gz
